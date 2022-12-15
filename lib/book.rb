f7f7dfa689352081edc3b3cae08cbd89da8f8ffe
Ryan <wrosullivan88@gmail.com>
2022-12-14 22:25:19 -0600

    complete

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,47 @@
+class Book
+
+    attr_reader :title
+    attr_accessor :author, :genre, :page_count #refactoring below code with attrs
 
 
+    def initialize (title)
+        @title = title
+    end
+
+    def title
+        @title
+    end
+
+    # def author=(author) #setter
+    #     @author = author
+    # end
+
+    # def author #getter
+    #     @author
+    # end
+
+    # def page_count=(pages)
+    #     @page_count = pages
+    # end
+
+    # def page_count
+    #     @page_count
+    # end
+
+    # def genre=(genre)
+    #     @genre=genre
+    # end
+
+    # def genre
+    #     @genre
+    # end
+
+    # def turn_page("Flipping the page...wow, you read fast!")
+    #     @turn_page
+    # end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
